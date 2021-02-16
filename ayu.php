dc46370789a3ac7ce306bb3493b816fe0bcf079d
rizkhal <lamaaurizkhal@gmail.com>
2021-02-17 04:26:07 +0900

    init :tada:

---
+++ ayu.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * ¯/_(ツ)_/¯
+ * 
+ * HOW TO USE IT ?
+ * type in console:
+ * php Skeleton.php --title="The amazing title" --cover="img id from https://i.imgflip.com"
+ */
+
+if (! function_exists('dd')) {
+    /**
+     * Die dump pretty print debugging
+     * @return array
+     */
+    function dd()
+    {
+        array_map(function($x) {
+                print_r($x);
+        }, func_get_args());
+        die;
+    }
+}
+
+class Ayu
+{
+    /**
+     * Filter arguments
+     * 
+     * @param void
+     */
+    public function __construct($argument)
+    {
+        preg_match('~^.+?\.php.--title=(.*).--cover=(.*)~', implode(' ', $argument), $matches);        
+
+        $this->build($matches);
+    }
+
+    /**
+     * Build skeleton
+     * 
+     * @param  array $argument
+     * @param  string $skeleton
+     * @return void
+     */
+    protected function build(array $matches): void
+    {
+        $id = $matches[2];
+        $title = $matches[1];
+        $slug = $this->generateSlug($title);
+        $stub = file_get_contents('stubs/ayu.stub');
+
+        $contents = $this->replaceCover($stub, $id, $slug)->replaceTitle($stub, $title);
+        file_put_contents("contents/{$slug}.md", $contents);
+    }
+
+    /**
+     * Replace thumbnail of posts filename
+     * 
+     * @param  string $filename 
+     * @param  string &$skeleton
+     * @return self          
+     */
+    protected function replaceCover(string &$stub, string $id, string $filename): self
+    {
+        $filename = $this->downloadCover($id, $filename);
+
+        $stub = str_replace(":cover", $filename, $stub);
+        return $this;
+    }
+    
+    /**
+     * Replace title
+     *
+     * @param string $stub
+     * @param string $title
+     * @return void
+     */
+    protected function replaceTitle(string &$stub, string $title): string
+    {
+        return str_replace(":title", $title, $stub);
+    }
+    
+    /**
+     * Download image
+     *
+     * @return string
+     */
+    public function downloadCover(string $id, string $filename): string
+    {
+        $cover = "assets/{$filename}.png";
+
+        try {
+            copy("https://i.imgflip.com/{$id}.jpg", $cover);
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+        }
+
+        return $cover;
+    }
+
+    /**
+     * Make slug of posts
+     * 
+     * @param  string $slug
+     * @return string
+     */
+    protected function generateSlug(string $filename): string
+    {
+        return str_replace(' ', '-', $filename);
+    }
+}
+
+/** exec */
+$skeleton = new Ayu($argv);
---
+++ contents/this-is-for-example.md
@@ -0,0 +1,3 @@
+<img src="../assets/this-is-for-example.png" alt="Thumbnail of content" align="center">
+<br />
+###this is for example
